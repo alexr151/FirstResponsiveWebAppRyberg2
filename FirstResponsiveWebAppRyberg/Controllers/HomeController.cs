476e5230adfcde546b81dc10cdcda5d16ff83ae0
alex <alex@DESKTOP-2CNKQT1>
2021-05-31 22:18:24 -0500

    Add project files.

---
+++ FirstResponsiveWebAppRyberg/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FirstResponsiveWebAppRyberg.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstResponsiveWebAppRyberg.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = 0;
+            ViewBag.Name = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AgeCalculator model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Age = model.AgeThisYear();
+                ViewBag.Name = model.Name;
+            } 
+                else
+            {
+                ViewBag.Age = 0;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppRyberg/Models/AgeCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstResponsiveWebAppRyberg.Models
+{
+    public class AgeCalculator
+    {
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Please enter what year you were born")]
+        [Range(1910, 2021, ErrorMessage = "Please enter a year between 1910 and now")]
+        public int? DOB { get; set; }
+        public int? AgeThisYear()
+        {
+            System.DateTime moment = DateTime.Now;
+
+            int? result = moment.Year - DOB;
+            return result;
+        }
+    }
+}
